08c2bae314f37256c5332c56942dc150daa1efd2
GUGGERINHO <gustav.palmqvist.02@gmail.com>
2026-01-15 17:39:28 +0100

    List stores with inventory and book titles

--- LABB_002/Program.cs
+++ LABB_002/Program.cs
@@ -1,10 +1,30 @@
 ﻿
 
 using LABB_002.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 using var db = new BokhandelContext();
 
-foreach  (var book in db.Books)
+var stores = db.Stores.ToList();
+
+foreach  (var store in stores)
 {
-    Console.WriteLine($"{book.Title}");
+    Console.WriteLine($"Store: {store.Name}");
+
+    var storeInventory = db.Inventories
+        .Where(i => i.StoreId == store.Id)
+        .Join(
+            db.Books,
+            i => i.Isbn13,
+            b => b.Isbn13,
+            (i, b) => new { Book = b, i.Quantity }
+        )
+        .ToList();
+
+    foreach (var item in storeInventory)
+    {
+        Console.WriteLine($"{item.Book.Title} | Quantity: {item.Quantity}");
+    }
+    Console.WriteLine();
 }
